c65391c6152c8ee62f28949cfad9a27acb9aed01
xjnny <jennylcrowther@gmail.com>
2017-05-16 14:40:50 +1200

    added form validation messages

--- main.js
+++ main.js
@@ -16,3 +16,35 @@ $(document).ready(function(){
 $(document).ready(function($) {
 			$('.my-slider').unslider();
 });
+
+
+
+$(document).ready(function() {
+  $('#contact-form').submit(function(e) {
+      var name = $('#inputName')
+      var email = $('#inputEmail')
+      var message = $('#inputMessage')
+    
+      if(name.val() == "" || email.val() == "" || message.val() == "") {
+        $('.submit-fail').fadeIn(400).delay(1500).fadeOut(400);
+        return false;
+      }
+      else {
+        $.ajax({
+          method: 'POST',
+          url: '//formspree.io/gilbycreative@gmail.com',
+          data: $('#contact-form').serialize(),
+          datatype: 'json'
+        });
+        e.preventDefault();
+        $(this).get(0).reset();
+        $('.submit-success').fadeIn(400).delay(1500).fadeOut(400);
+		$('.submit-fail').fadeToggle(400);
+      }
+    });
+  
+  $('.submit-fail, .submit-success').click(function() {
+    $(this).hide();
+  })
+});
+  

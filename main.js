0b96efad2476cee122a6f9e0866cba203efb9b05
xjnny <jennylcrowther@gmail.com>
2017-05-17 23:57:16 +1200

    added in more stuff

--- main.js
+++ main.js
@@ -1,53 +1,47 @@
-$(document).ready(function(){
+$(document).ready(function() {
   var original = $('.character-original');
   var replacement = $('.character-replacement');
   
-  $('.character-wrapper').mouseover(function(){
-        $(this).find(original).css("display", "none");
-        $(this).find(replacement).css("display", "inline");
-    });
-  $('.character-wrapper').mouseout(function(){
-        $(this).find(original).css("display", "inline");
-        $(this).find(replacement).css("display", "none");
-    });
+  $('.character-wrapper').mouseover(function() {
+    $(this).find(original).css("display", "none");
+    $(this).find(replacement).css("display", "inline");
+  });
+  $('.character-wrapper').mouseout(function() {
+    $(this).find(original).css("display", "inline");
+    $(this).find(replacement).css("display", "none");
+  });
 });
-
 
 $(document).ready(function($) {
-			$('.my-slider').unslider({
-			infinite: true
-			});
-	
+  $('.my-slider').unslider({
+    infinite: true
+  });
+
 });
-
-
 
 $(document).ready(function() {
   $('#contact-form').submit(function(e) {
-      var name = $('#inputName')
-      var email = $('#inputEmail')
-      var message = $('#inputMessage')
-    
-      if(name.val() == "" || email.val() == "" || message.val() == "") {
-        $('.submit-fail').fadeIn(400).delay(1500).fadeOut(400);
-        return false;
-      }
-      else {
-        $.ajax({
-          method: 'POST',
-          url: '//formspree.io/gilbycreative@gmail.com',
-          data: $('#contact-form').serialize(),
-          datatype: 'json'
-        });
-        e.preventDefault();
-        $(this).get(0).reset();
-        $('.submit-success').fadeIn(400).delay(1500).fadeOut(400);
-		$('.submit-fail').hide;
-      }
-    });
-  
+    var name = $('#inputName')
+    var email = $('#inputEmail')
+    var message = $('#inputMessage')
+
+    if (name.val() == "" || email.val() == "" || message.val() == "") {
+      $('.submit-fail').fadeIn(400).delay(1500).fadeOut(400);
+      return false;
+    } else {
+      $.ajax({
+        method: 'POST',
+        url: '//formspree.io/gilbycreative@gmail.com',
+        data: $('#contact-form').serialize(),
+        datatype: 'json'
+      });
+      e.preventDefault();
+      $(this).get(0).reset();
+      $('.submit-success').fadeIn(400).delay(1500).fadeOut(400);
+      $('.submit-fail').hide;
+    }
+  });
   $('.submit-fail, .submit-success').click(function() {
     $(this).hide();
   })
 });
-  

a2b70a39f1b17ce1f0ff930ad82dcb33cdd9d4a1
xjnny <jennylcrowther@gmail.com>
2017-05-18 14:26:04 +1200

    pre fullpage.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var original = $('.character-original');
   var replacement = $('.character-replacement');
-  
+
   $('.character-wrapper').mouseover(function() {
     $(this).find(original).css("display", "none");
     $(this).find(replacement).css("display", "inline");
@@ -45,3 +45,36 @@ $(document).ready(function() {
     $(this).hide();
   })
 });
+
+
+// $(document).ready(function() {
+// 	$('#fullpage').fullpage();
+// });
+
+
+// var width = $('.underline').width();
+//
+// var steps = 10;
+// var height = 5;
+// var step_size = width/steps;
+//
+// var d_animated = ['M0', '1'];
+// var d_normal = ['M0', '1'];
+//
+// for (var i=1; i<=steps; i++) {
+//   d_normal.push(step_size*(-0.5 + i), 1, step_size*i, 1);
+//   d_animated.push(step_size*(-0.5 + i), height, step_size*i, 1);
+// }
+//
+// $(document).ready(function() {
+//   $('.underline--path').attr('d', d_normal.join(' '));
+//
+//   $('.link').hover(function() {
+//     $('.underline--animation').attr({from: d_normal.join(' '), to: d_animated.join(' ')})
+//   	$('.underline--animation')[0].beginElement();
+//   }, function() {
+//     $('.underline--animation').attr({to: d_normal.join(' '), from: d_animated.join(' ')})
+//   	$('.underline--animation')[0].beginElement();
+//   });
+//
+// });
